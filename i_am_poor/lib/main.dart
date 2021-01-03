cd5da87bca816b45b2003d1f2b7583203bf35b5e
Patrice Tisserand <p.tisserand@gmail.com>
2021-01-03 18:53:23 +0100

    Added 'I am poor' udemy section 5 App challenge

---
+++ i_am_poor/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.lightBlueAccent,
+        appBar: AppBar(
+          backgroundColor: Colors.amber,
+          title: Text('I am poor'),
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/surfboard.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
